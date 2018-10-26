8b95ca99ce1f218e97cd4ee1d79d118507fc0bb0
darth-c0d3r <gurparkashsohi@gmail.com>
2018-10-26 20:55:15 +0530

    random

--- train.py
+++ train.py
@@ -30,70 +30,73 @@ optimizer = optim.Adagrad(model.parameters(), lr=0.005)
 
 def train(model, optim, db):
 
-	for epoch in range(1, epochs+1):
+	# for epoch in range(1, epochs+1):
 
 		train_loader = torch.utils.data.DataLoader(db['train'],batch_size=batch_size, shuffle=True)
-
+		print(enumerate(train_loader))
 		# Update (Train)
-		model.train()
-		for batch_idx, (data, target) in enumerate(train_loader):
+	# 	model.train()
+	# 	for batch_idx, (data, target) in enumerate(train_loader):
 
-			data, target = Variable(data), Variable(target)
-			optimizer.zero_grad()
-			output = model(data)
-			loss = criterion(output,target)
-			loss.backward()
-			optimizer.step()
+	# 		# print(data.size())
 
-			if batch_idx % report_every == 0:
-				print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-					epoch, batch_idx * len(data), len(train_loader.dataset),
-					100. * batch_idx / len(train_loader), loss.item()))
+	# 		data, target = Variable(data), Variable(target)
+	# 		optimizer.zero_grad()
+	# 		output = model(data)
+	# 		loss = criterion(output,target)
+	# 		loss.backward()
+	# 		optimizer.step()
+
+	# 		if batch_idx % report_every == 0:
+	# 			print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+	# 				epoch, batch_idx * len(data), len(train_loader.dataset),
+	# 				100. * batch_idx / len(train_loader), loss.item()))
 
 
-		# Evaluate
-		model.eval()
-		for name in ['train', 'eval']:
-			test_loss = float(0)
-			correct = 0
-			test_loader = torch.utils.data.DataLoader(db[name], batch_size=batch_size, shuffle=True)
-			for data, target in test_loader:
-				with torch.no_grad():
-					data = Variable(data)
-				target = Variable(target)
-				output = model(data)
-				test_loss += criterion(output, target).item() # sum up batch loss
-				pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
-				correct += pred.eq(target.data.view_as(pred)).cpu().sum()
-			test_loss /= len(test_loader.dataset)
-			accuracy = float(correct) / len(test_loader.dataset)
+	# 	# Evaluate
+	# 	model.eval()
+	# 	for name in ['train', 'eval']:
+	# 		test_loss = float(0)
+	# 		correct = 0
+	# 		test_loader = torch.utils.data.DataLoader(db[name], batch_size=batch_size, shuffle=True)
+	# 		for data, target in test_loader:
+	# 			with torch.no_grad():
+	# 				data = Variable(data)
+	# 			target = Variable(target)
+	# 			output = model(data)
+	# 			test_loss += criterion(output, target).item() # sum up batch loss
+	# 			pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+	# 			correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+	# 		test_loss /= len(test_loader.dataset)
+	# 		accuracy = float(correct) / len(test_loader.dataset)
 
-			print(name+' set: Average loss: {:.4f}, Accuracy: {}/{} ({:.6f})'.format(
-				test_loss, correct, len(test_loader.dataset),
-				accuracy))
-		print('')
+	# 		print(name+' set: Average loss: {:.4f}, Accuracy: {}/{} ({:.6f})'.format(
+	# 			test_loss, correct, len(test_loader.dataset),
+	# 			accuracy))
+	# 	print('')
 
-	model.eval()
-	tally_1 = [0]*n_classes
-	tally_2 = [0]*n_classes
-	test_loader = torch.utils.data.DataLoader(db['eval'], batch_size=batch_size, shuffle=False)
-	for data, target in test_loader:
-		with torch.no_grad():
-			data = Variable(data)
-		target = Variable(target)
-		output = model(data)
-		pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
-		for i in range(pred.size()[0]):
-			# print("%d,%d"%(target[i].item(), pred[i][0].item()))
-			tally_1[target[i].item()] += (target[i].item() == pred[i][0].item())
-			tally_2[pred[i][0].item()] += (target[i].item() != pred[i][0].item())
-	print(tally_1)
-	print(tally_2)
+	# model.eval()
+	# tally_1 = [0]*n_classes
+	# tally_2 = [0]*n_classes
+	# test_loader = torch.utils.data.DataLoader(db['eval'], batch_size=batch_size, shuffle=False)
+	# for data, target in test_loader:
+	# 	with torch.no_grad():
+	# 		data = Variable(data)
+	# 	target = Variable(target)
+	# 	output = model(data)
+	# 	pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+	# 	for i in range(pred.size()[0]):
+	# 		# print("%d,%d"%(target[i].item(), pred[i][0].item()))
+	# 		tally_1[target[i].item()] += (target[i].item() == pred[i][0].item())
+	# 		tally_2[pred[i][0].item()] += (target[i].item() != pred[i][0].item())
+	# print(tally_1)
+	# print(tally_2)
 
 def main():
 	dataset = getFacesDataset(n_images,n_classes,total_images)
 	# print(dataset['train'][0][0].size())
 	train(model,optim,dataset)
+	# print(dataset['train'][0])
 	torch.save(model, 'trained_model.pt')
 
 if __name__ == '__main__':
